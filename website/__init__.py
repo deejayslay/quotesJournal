602c9cfceb891faa9f0a87224ae7400624aaaec2
deejayslay <drdeeejay@gmail.com>
2023-05-06 15:29:31 -0700

    Set up base html

--- website/__init__.py
+++ website/__init__.py
@@ -9,4 +9,12 @@ def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "qjwebsite"
 
+    # import blueprints
+    from .views import views
+    from .auth import auth
+
+    # register blueprints with Flask app
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
     return app
