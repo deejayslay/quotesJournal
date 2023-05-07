9f2ac27d6d13cf541b9feb7c290a2e982c516003
deejayslay <drdeeejay@gmail.com>
2023-05-06 18:10:59 -0700

    Create Database

--- website/__init__.py
+++ website/__init__.py
@@ -4,6 +4,7 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 # define database
 db = SQLAlchemy()
@@ -24,4 +25,17 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    # models defined before creating database
+    from .models import User, Quote, Note
+
+    create_database(app)
+
     return app
+
+
+# checks if database exists; if it doesn't, db is created
+def create_database(app):
+    if not path.exists("website/" + DB_NAME):
+        with app.app_context():
+            db.create_all()
+            print("Created Database!")
